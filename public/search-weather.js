09c7605950319b354cb3a9e1f6ac09db582b8d80
Freddy Montano <freddymx@gmail.com>
2017-12-04 22:09:05 -0500

    fixed stuff

--- public/search-weather.js
+++ public/search-weather.js
@@ -4,11 +4,19 @@ $(function() {
     
     function getLocation(e) {
         e.preventDefault();
+
+        var location = $("#search_weather").val()
+
         // var location = $('#search_weather').val();
-        $.get('api/location-api.js', displayLocation);
+        $.get('api/location/?location=' + location, displayLocation);
     }
     
-    function displayLocation(loc) {
-        $('.location').html(loc);
+    function displayLocation(response) {
+    	console.log(response)
+        $('.location').html(response.location);
+        $('#temperature').html(response.weather.temperature);
+        $('#summary').html(response.weather.summary);
+        $('#apparentTemperature').html(response.weather.apparentTemperature);
+        $('#hourlySummary').html(response.weather.hourlySummary);
     }
 });
